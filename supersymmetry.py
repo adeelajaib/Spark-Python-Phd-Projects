60d9f3306082cc5241594cc5c65bd454fcfafcfb
adeelajaib <adeel@udel.edu>
2016-01-23 00:14:46 -0800

    Update supersymmetry.py

--- supersymmetry.py
+++ supersymmetry.py
@@ -1,3 +1,5 @@
+#this program calculates the mass of the lightest particle (stau) in the dataset "dataset-supersymmetry-1.txt"
+
 from pyspark import SparkConf, SparkContext
 import collections
 
@@ -7,11 +9,11 @@ sc = SparkContext(conf = conf)
 def parseLine(line):
     fields = line.split()
     StauMass = float(fields[1])
-    return (1,StauMass)
+    return (1,StauMass)    
 
 lines = sc.textFile("dataset-supersymmetry-1.txt")
-stau = lines.map(parseLine)
-lighteststau=stau.reduceByKey(lambda x,y: min(x,y))
+stau = lines.map(parseLine)     #gives the mass as a list key-value pairs
+lighteststau=stau.reduceByKey(lambda x,y: min(x,y))       #find the minimum value
 results = stau.collect()
 
 
